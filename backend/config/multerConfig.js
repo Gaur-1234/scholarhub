3cbfdb69994c9e82a8619b599dd18fdadefbcf7d
Gaurank Verma <gaurankverma33@gmail.com>
2026-06-05 01:35:16 +0530

    uploads issue resolved

--- backend/config/multerConfig.js
+++ backend/config/multerConfig.js
@@ -1,22 +1,20 @@
 const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const uploadDir = path.join(__dirname, "../uploads");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-
   destination: (req, file, cb) => {
-
-    cb(null, "uploads/");
-
+    cb(null, path.join(__dirname, "../uploads"));
   },
 
   filename: (req, file, cb) => {
-
-    cb(
-      null,
-      Date.now() + "-" + file.originalname
-    );
-
+    cb(null, Date.now() + "-" + file.originalname);
   }
-
 });
 
 const upload = multer({

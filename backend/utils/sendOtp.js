1faa9c6826deb01cb735b137aa236f4359d67560
Gaurank Verma <gaurankverma33@gmail.com>
2026-06-06 10:03:36 +0530

    mail port changed

--- backend/utils/sendOtp.js
+++ backend/utils/sendOtp.js
@@ -1,92 +1,121 @@
 const nodemailer = require("nodemailer");
 
 const sendOtp = async (email, otp) => {
+
   try {
-   const transporter = nodemailer.createTransport({
 
-  host: "smtp.gmail.com",
+    const transporter = nodemailer.createTransport({
 
-  port: 587,
+      host: "smtp.gmail.com",
 
-  secure: false,
+      port: 587,
 
-  auth: {
+      secure: false,
 
-    user: process.env.EMAIL_USER,
+      auth: {
 
-    pass: process.env.EMAIL_PASS,
+        user: process.env.EMAIL_USER,
 
-  },
+        pass: process.env.EMAIL_PASS
 
-  tls: {
+      },
 
-    rejectUnauthorized: false
+      tls: {
 
-  }
+        rejectUnauthorized: false
 
-});
+      }
+
+    });
+
+    // SMTP TEST
+
+    await transporter.verify();
+
+    console.log(
+      "SMTP SERVER READY"
+    );
+
     const mailOptions = {
+
       from: process.env.EMAIL_USER,
 
       to: email,
 
-      subject: "ScholarHub Security Verification Code",
+      subject:
+      "ScholarHub Security Verification Code",
 
       html: `
 
-  <div
-    style="
-      font-family: Arial;
-      padding: 20px;
-    "
-  >
+      <div
+        style="
+          font-family: Arial;
+          padding: 20px;
+        "
+      >
 
-    <h1
-      style="
-        color:#2563eb;
-      "
-    >
-      ScholarHub
-    </h1>
+        <h1
+          style="
+            color:#2563eb;
+          "
+        >
+          ScholarHub
+        </h1>
 
+        <p>
+          Your verification code is:
+        </p>
 
-    <p>
-      Your verification code is:
-    </p>
+        <h2
+          style="
+            letter-spacing:4px;
+          "
+        >
+          ${otp}
+        </h2>
 
+        <p>
+          This OTP expires in
+          5 minutes.
+        </p>
 
-    <h2
-      style="
-        letter-spacing:4px;
-      "
-    >
-      ${otp}
-    </h2>
+        <p>
+          If you did not request
+          this code, please ignore
+          this email.
+        </p>
 
+      </div>
 
-    <p>
-      This OTP expires in
-      5 minutes.
-    </p>
+      `
 
-
-    <p>
-      If you did not request
-      this code, please ignore
-      this email.
-    </p>
-
-  </div>
-
-`,
     };
 
-    await transporter.sendMail(mailOptions);
+    const info =
+    await transporter.sendMail(
+      mailOptions
+    );
 
-    console.log("OTP Email Sent");
-  } catch (error) {
-    console.log(error);
+    console.log(
+      "OTP EMAIL SENT:",
+      info.messageId
+    );
+
+    return true;
+
   }
+
+  catch (error) {
+
+    console.error(
+      "OTP MAIL ERROR:",
+      error
+    );
+
+    throw error;
+
+  }
+
 };
 
 module.exports = sendOtp;

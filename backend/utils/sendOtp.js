556343928e53bf31bf985a657da14d2a3547c992
Gaurank Verma <gaurankverma33@gmail.com>
2026-06-06 09:47:05 +0530

    mail port changed

--- backend/utils/sendOtp.js
+++ backend/utils/sendOtp.js
@@ -2,16 +2,29 @@ const nodemailer = require("nodemailer");
 
 const sendOtp = async (email, otp) => {
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
+   const transporter = nodemailer.createTransport({
 
-      auth: {
-        user: process.env.EMAIL_USER,
+  host: "smtp.gmail.com",
 
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  port: 587,
 
+  secure: false,
+
+  auth: {
+
+    user: process.env.EMAIL_USER,
+
+    pass: process.env.EMAIL_PASS,
+
+  },
+
+  tls: {
+
+    rejectUnauthorized: false
+
+  }
+
+});
     const mailOptions = {
       from: process.env.EMAIL_USER,
 
